931462aa185d375340999d064c53a85fc66ba9ac
andjar <ajarmund@gmail.com>
2020-04-21 01:15:55 +0200

    Added a space to improve string replacement

--- roam/archive.php
+++ roam/archive.php
@@ -2,7 +2,7 @@
 
     $path_to_file = $_POST['fpath'];
     $file_contents = file_get_contents($path_to_file);
-    $file_contents = str_replace("{{tag>note","{{tag>archived",$file_contents);
+    $file_contents = str_replace("{{tag>note ","{{tag>archived ",$file_contents);
     file_put_contents($path_to_file,$file_contents);
     header("Location: doku.php?id=" . $_POST['pageid']);
 

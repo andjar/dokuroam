4810c20d33e1f8038c924c60bc70700fb3bc85ef
andjar <ajarmund@gmail.com>
2020-04-23 02:08:51 +0200

    New article template with YAML and correspoding improvement of pandoc export

--- roam/pandoc.php
+++ roam/pandoc.php
@@ -1,14 +1,53 @@
 <?php
 
-        $data = substr($_POST['fpath'],0,-4) . '/references.txt';
-        $current = file_get_contents($data);
+       $refPath = substr($_POST['fpath'],0,-4) . '/references.txt';
+       $ref = file_get_contents($refPath);
         // Append a new person to the file
-        $data = substr($data,0,-4) . '.bib';
+       $refPath = $_SERVER['DOCUMENT_ROOT'] . '/data/tmp/' . $_POST['pageid'] . '.bib';
         // Write the contents back to the file
-        $result = file_put_contents($data, $current);
+       file_put_contents($refPath, $ref);
 
-    $run_str = 'pandoc --bibliography="' . $data . '" --filter pandoc-citeproc -f markdown ' . $_POST['fpath'] . ' -o ' . $_SERVER['DOCUMENT_ROOT'] . '/data/tmp/' . $_POST['pageid'] .'.docx';
+       $textPath = $_POST['fpath'];
+       $text = file_get_contents($textPath);
+
+       $lines = explode(PHP_EOL, $text);
+       $insideWRAP = false;
+       $leftWRAP = false;
+       $newWRAPs = 0;
+       foreach($lines as $key => $line){
+           if( $insideWRAP == false && (strcmp($line, '<WRAP yaml>') == 0)){
+               $insideWRAP = true;
+               $lines[$key] = '---';
+           }else{
+               if($insideWRAP){
+                   if(strpos($line, '</WRAP>') !== false){
+                       if($newWRAPs > 0){
+                           unset($lines[$key]);// = '';
+                           $newWRAPs--;
+                       }else{
+                           $lines[$key] = '---';
+                           $insideWRAP = false;
+                           $leftWRAP = true;
+                       }
+                   } elseif (strpos($line, '<WRAP') !== false){
+                       unset($lines[$key]);// = '';
+                       $newWRAPs++;
+                   } else {
+                       $lines[$key] = strip_tags($line);
+                       if(strpos($lines[$key], 'to: ') !== false){
+                           $toFormat = substr($lines[$key], 4);
+                       }
+                   }
+               }
+           }
+       }
+
+       $text = implode(PHP_EOL, $lines);
+       $mdPath = $_SERVER['DOCUMENT_ROOT'] . '/data/tmp/' . $_POST['pageid'] . '.md';
+       file_put_contents($mdPath, $text);
+
+       $run_str = 'pandoc --bibliography="'. $refPath .'" --filter pandoc-citeproc "' . $mdPath . '" -o "' . $_SERVER['DOCUMENT_ROOT'] . '/data/tmp/' . $_POST['pageid'] .'.' . $toFormat .'"';
     exec($run_str);
-    header("Location: ". '/data/tmp/' . $_POST['pageid'] .'.docx');
+    header("Location: ". '/data/tmp/' . $_POST['pageid'] .'.' . $toFormat);
 
 ?>

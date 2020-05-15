f9b4841e0529659462519ec1a341f5239adb2efc
andjar <ajarmund@gmail.com>
2020-05-15 03:39:20 +0200

    Updated standard configurations

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+admin:$1$GdB5xuw0$r8RRGzHkqAkgjyP95Z/AP.:admin:admin@admin.com:admin,user
